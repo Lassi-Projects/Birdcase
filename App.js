b615d275693e8123056b8980ae50e882abc577e2
Lassi Valtari <lassi.m.valtari@utu.fi>
2019-01-18 11:01:02 +0200

    Basic layout and colors for main menu

--- App.js
+++ App.js
@@ -2,14 +2,94 @@
 by Lassi Valtari*/
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, ScrollView, StyleSheet, Text, View} from 'react-native';
 
+/*
+Styles of the components:
+Main menu in 3 parts: title, list and add-button
+Add form: TODO
+*/
+const styles = StyleSheet.create ({
+  /*Main menu components*/
+  mainmenu: {
+    flex: 1,
+  },
 
-export default class App extends Component {
+  //Title and it's container
+  _appTitleContainer:{
+    height: 50,
+    alignItems: 'center',
+    backgroundColor: '#336419',
+    padding: (50 - 20)/2, 
+  },
+  _appTitle: {
+    color: 'whitesmoke',
+    fontSize: 20,
+  },
+
+  //List of Observations
+  _birdList: {
+    flex:1,
+    backgroundColor: 'whitesmoke',
+  },
+
+  //Add button on the bottom of screen
+  _addButton: {
+    height: 35,
+  },
+
+  /*Add form components*/
+  //TODO
+})
+
+/*Handles observation adding form as seperate class - TODO*/
+class ObservationForm extends Component {
   render() {
     return (
       <View>
-        <Text>"Hello World!"</Text>
+        <Text>"You're in form view!"</Text>
+      </View>
+    );
+  }
+}
+
+/*Core app including main menu */
+export default class App extends Component {
+  _openForm() {
+    Alert.alert('Creating form...');
+  }
+
+  render() {
+    return (
+      <View style={styles.mainmenu}>
+        <View id="Title" style={styles._appTitleContainer}>
+          <Text style={styles._appTitle}>BIRDCASE</Text>
+        </View>
+        <View id="BirdsList" style={styles._birdList}>
+          <ScrollView>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+            <Text style={{fontSize: 30}}>List of birds here!</Text>
+          </ScrollView>
+        </View>
+        <View id="AddNewButton" style={styles._addButton}>
+          <Button
+          color='#a7364f'
+          onPress={this._openForm}
+          title="+ ADD NEW"
+          />
+        </View>
       </View>
     );
   }
