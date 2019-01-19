2b2b6b04d8540a6b6df26a1ff342fb7ea57c955c
Lassi Valtari <lassi.m.valtari@utu.fi>
2019-01-19 21:42:52 +0200

    List sort added. Dialogbox for rarity

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 /**Birdwatching project for CGI Future Talent
 *by Lassi Valtari*/
 
-import React, {Component} from 'react';
-import {Alert, Button, FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator, createAppContainer, NavigationEvents} from 'react-navigation';
-import {TextInput} from 'react-native-gesture-handler';
-import {List, ListItem} from "react-native-elements";
+import React, { Component } from 'react';
+import { Alert, Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer, NavigationEvents } from 'react-navigation';
+import { TextInput } from 'react-native-gesture-handler';
+import { List, ListItem } from "react-native-elements";
+import Dialog from "react-native-dialog";
 
 /**
 *Styles of the components:
 *Main menu in 3 parts: title, list and add-button
 *Add form: TODO
 */
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   /*Main menu components*/
   mainmenu: {
     flex: 1,
@@ -27,7 +28,7 @@ const styles = StyleSheet.create ({
 
   //List of Observations
   _birdListBackground: {
-    flex:1,
+    flex: 1,
     backgroundColor: 'whitesmoke',
   },
   _birdList: {
@@ -48,7 +49,7 @@ var observationArray = new Array();
 
 /**Home screen*/
 class HomeScreen extends React.Component {
-  
+
   static navigationOptions = {
     title: 'BIRDCASE',
   };
@@ -64,9 +65,29 @@ class HomeScreen extends React.Component {
     };
   }
 
+  sortObservations(list, by) {
+    var sortedList = new Array();
+    if(by == 'Date') {
+      while(list.length > 0) {
+        var index = 0;
+        for(var i = 0; i < list.length; ++i) {
+          if(list[i].time.getTime() > list[index].time.getTime()) {
+            list[index] = list[i];
+            index = i;
+          }
+        }
+        sortedList.push(list[index]);
+        list.splice(index, 1)
+      }
+    };
+    list = sortedList;
+    return list;
+  }
+
   componentDidMount() {
-    this.getTheData(function(json) {
+    this.getTheData(function (json) {
       observationArray = json;
+      observationArray = this.sortObservations(observationArray, 'Date'),
       this.setState({
         data: observationArray,
         refreshing: false,
@@ -96,9 +117,11 @@ class HomeScreen extends React.Component {
       refreshing: true,
       seed: this.state.seed + 1,
     },
-    function () {this.setState({
-      refreshing: false,
-    })}
+      function () {
+        this.setState({
+          refreshing: false,
+        })
+      }
     );
   };
 
@@ -108,42 +131,42 @@ class HomeScreen extends React.Component {
 
   render() {
 
-    var currentList =(this.state.loading)?<View/>:
+    var currentList = (this.state.loading) ? <View /> :
       <FlatList
-        keyExtractor={observation => {observation.time.getTime().toString()}}
-        data = {this.state.data}
-        refreshing ={this.state.refreshing}
+        keyExtractor={observation => { observation.time.getTime().toString() }}
+        data={this.state.data}
+        refreshing={this.state.refreshing}
 
         onRefresh={this.onRefresh}
 
-        renderItem= {({item:observation}) => (
+        renderItem={({ item: observation }) => (
           <ListItem
-            component ={View}
-            title={<View style={{flex: 1, height: 30}}>
-              <View style={{flex: 1}}><Text style={{fontSize: 20}}>{observation.name}</Text></View>
-              
+            component={View}
+            title={<View style={{ flex: 1, height: 30 }}>
+              <View style={{ flex: 1 }}><Text style={{ fontSize: 20 }}>{observation.name}</Text></View>
+
             </View>}
-            subtitle= {<View style={{flex: 1, flexDirection: 'column'}}>
-              <View style={{flex: 1, flexDirection: 'row'}}>
-                <View style={{flex: 1}}><Text>{observation.time.getHours() + ":"
-                + observation.time.getMinutes() + " " 
-                + observation.time.toLocaleDateString()}</Text></View>
-                <View style={{flex: 1}}><Text>{observation.rarity}</Text></View>
-               </View>
-              <View style={{flex: 2}}><Text>{observation.notes}</Text></View>
+            subtitle={<View style={{ flex: 1, flexDirection: 'column' }}>
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={{ flex: 1 }}><Text>{observation.time.getHours() + ":"
+                  + observation.time.getMinutes() + " "
+                  + observation.time.toLocaleDateString()}</Text></View>
+                <View style={{ flex: 1 }}><Text>{observation.rarity}</Text></View>
+              </View>
+              <View style={{ flex: 2 }}><Text>{observation.notes}</Text></View>
             </View>
-              
-              }
+
+            }
           />
         )}
-        ></FlatList>
+      ></FlatList>
 
     return (
-      
+
       /**Home screen uses mainmenu styles */
       <View style={styles.mainmenu}>
         {/**List of all the birds*/}
-        <View  style={styles._birdListBackground}>
+        <View style={styles._birdListBackground}>
           <ScrollView>
             {currentList}
           </ScrollView>
@@ -151,9 +174,9 @@ class HomeScreen extends React.Component {
         {/*Button to add new bird in the list*/}
         <View style={styles._addButton}>
           <Button
-          color='#a7364f'
-          onPress={() => this.props.navigation.navigate('AddForm', {onNavigateBack: this.handleOnNavigateBack})}
-          title="+ ADD NEW"
+            color='#a7364f'
+            onPress={() => this.props.navigation.navigate('AddForm', { onNavigateBack: this.handleOnNavigateBack })}
+            title="+ ADD NEW"
           />
         </View>
       </View>
@@ -167,64 +190,111 @@ class AddFormScreen extends React.Component {
     title: 'Add new observation',
   };
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
       name: 'None',
       rarity: 'Common',
       notes: 'None',
+      rarityDialogVisible: false,
+      refreshing: false,
+      seed: 0,
     }
 
   }
 
+  onRefresh = () => {
+    this.setState({
+      refreshing: true,
+      seed: this.state.seed + 1,
+    },
+      function () {
+        this.setState({
+          refreshing: false,
+        })
+      }
+    );
+  };
 
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{flex: 1, padding: 40, paddingTop: 20}}>
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{ flex: 1, padding: 40, paddingTop: 20, justifyContent: 'space-between'}}>
           <ScrollView >
             <TextInput
-              onChangeText ={(text) => {this.setState({name: text})}}
-              style={{height: 40, borderBottomWidth: 1, borderColor: 'grey'}}
-              placeholder = "Name of the species"
+              onChangeText={(text) => { this.setState({ name: text }) }}
+              style={{ height: 40, borderBottomWidth: 1, borderColor: 'grey' }}
+              placeholder="Name of the species"
             ></TextInput>
+            <View style={{height: 10}}/>
+            <Button
+              title="Select rarity"
+              padding={5}
+              color='#a7364f'
+              onPress={() => {this.setState({
+                rarityDialogVisible: true,
+              })}}
+            ></Button>
+            <View style={{height: 10}}/>
+            <Dialog.Container 
+              visible={this.state.rarityDialogVisible}
+              refreshing = {this.state.refreshing}
+              onRefresh={this.onRefresh}>
+              <Dialog.Title>Choose observation rarity:</Dialog.Title>
+              <Dialog.Button label="Common"
+                onPress={() => {
+                  this.setState({
+                    rarity: 'Common',
+                    rarityDialogVisible: false,
+                  })
+                }} />
+              <Dialog.Button label="Rare"
+                onPress={() => {
+                  this.setState({
+                    rarity: 'Rare',
+                    rarityDialogVisible: false,
+                  })
+                }} />
+              <Dialog.Button label="Extremely rare"
+                onPress={() => {
+                  this.setState({
+                    rarity: 'Extremely rare',
+                    rarityDialogVisible: false,
+                  })
+                }} />
+            </Dialog.Container>
             <TextInput
-              onChangeText ={(text) => {this.setState({rarity: text})}}
-              style={{height: 40, borderBottomWidth: 1, borderColor: 'grey'}}
-              placeholder = "Rarity"
-            ></TextInput>
-            <TextInput
-              onChangeText ={(text) => {this.setState({notes: text})}}
-              multiline= {true}
-              style={{borderBottomWidth: 1, borderColor: 'grey'}}
-              placeholder = "Notes"
+              onChangeText={(text) => { this.setState({ notes: text }) }}
+              multiline={true}
+              style={{ borderBottomWidth: 1, borderColor: 'grey' }}
+              placeholder="Notes"
             ></TextInput>
           </ScrollView>
         </View>
         <View style={styles._addButton}>
-        {/**Save button */}
-        <Button
-        color='#a7364f'
+          {/**Save button */}
+          <Button
+            color='#a7364f'
 
-        /**Save clicked*/
-        onPress={() => {
-          //Add observation to the Array
-          newObs = {
-            name: this.state.name, 
-            rarity: this.state.rarity, 
-            notes: this.state.notes,
-            time: new Date(),
-          }
-          observationArray.push(newObs)
-          //Go back to main
-          this.props.navigation.state.params.onNavigateBack()
-          this.props.navigation.goBack()
-        }}
-        title="SAVE"
-        />
+            /**Save clicked*/
+            onPress={() => {
+              //Add observation to the Array
+              newObs = {
+                name: this.state.name,
+                rarity: this.state.rarity,
+                notes: this.state.notes,
+                time: new Date(),
+              }
+              observationArray.push(newObs)
+              //Go back to main
+              this.props.navigation.state.params.onNavigateBack()
+              this.props.navigation.goBack()
+            }}
+            title="SAVE"
+          />
+        </View>
       </View>
-    </View>
     );
   }
 }
